84516c332fe29cbde849026030d7befc715c1f69
axiom-automation <hello@axiom.co>
2021-03-10 14:52:14 +0100

    Brew formula update for axiom version v0.1.12

--- Formula/axiom.rb
+++ Formula/axiom.rb
@@ -5,25 +5,25 @@
 class Axiom < Formula
   desc "Powerful log analytics from the comfort of your command-line"
   homepage "https://axiom.co"
-  version "0.1.11"
+  version "0.1.12"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/axiomhq/cli/releases/download/v0.1.11/axiom_0.1.11_darwin_amd64.tar.gz"
-    sha256 "6f0669fb9c80c970da6e55f2ff5a0aaadb226a810280662903fc71b748fbe339"
+    url "https://github.com/axiomhq/cli/releases/download/v0.1.12/axiom_0.1.12_darwin_amd64.tar.gz"
+    sha256 "4ff12ab3ff063a8d3b0c22956e915a32d283e34d997449a4f4b0a66750e9472b"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/axiomhq/cli/releases/download/v0.1.11/axiom_0.1.11_darwin_arm64.tar.gz"
-    sha256 "86d72fc284903f6d6f4fbdb4e9c05924599a75e25d89aa4d2e9f02a39541090a"
+    url "https://github.com/axiomhq/cli/releases/download/v0.1.12/axiom_0.1.12_darwin_arm64.tar.gz"
+    sha256 "eeb0493fe7434a33c29df7388b02964c7c8f72337841149c0bf414af62130068"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/axiomhq/cli/releases/download/v0.1.11/axiom_0.1.11_linux_amd64.tar.gz"
-    sha256 "bdfd12d9eb773990e44a787a3daea685ca5f73f501fb3a2bcebdca9f5674db86"
+    url "https://github.com/axiomhq/cli/releases/download/v0.1.12/axiom_0.1.12_linux_amd64.tar.gz"
+    sha256 "07eca3bc113042ee1763ea6877a7321c92f8338b233e1eaa67c2761c30ab7878"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/axiomhq/cli/releases/download/v0.1.11/axiom_0.1.11_linux_arm64.tar.gz"
-    sha256 "78197f154e6112781dd7e09fc9894e80dde1afbd4ef8a2faf03d38c65e645d5c"
+    url "https://github.com/axiomhq/cli/releases/download/v0.1.12/axiom_0.1.12_linux_arm64.tar.gz"
+    sha256 "237cb995453fa7f42c8b4c4c0be2e9cc72df15638aab692028dfa74b0c08ea64"
   end
 
   def install
