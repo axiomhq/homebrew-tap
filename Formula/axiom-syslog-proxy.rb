af1410227eb4040c67f11f996f9832a9c3d5663f
axiom-automation <hello@axiom.co>
2021-03-24 17:00:13 +0100

    Brew formula update for axiom-syslog-proxy version v0.1.3

--- Formula/axiom-syslog-proxy.rb
+++ Formula/axiom-syslog-proxy.rb
@@ -5,29 +5,29 @@
 class AxiomSyslogProxy < Formula
   desc "Syslog ingestion by proxy for Axiom"
   homepage "https://axiom.co"
-  version "0.1.1"
+  version "0.1.3"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.1/axiom-syslog-proxy_0.1.1_darwin_amd64.tar.gz"
-    sha256 "e088ba3015a07e73f429d70d16d09545090b2e13cf22cfc606fa7704cd55dab4"
+    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.3/axiom-syslog-proxy_0.1.3_darwin_amd64.tar.gz"
+    sha256 "284b72b2aaab9a563d24a32bf61c153bd7a7f5df249ba7c10e727210bf3bb452"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.1/axiom-syslog-proxy_0.1.1_darwin_arm64.tar.gz"
-    sha256 "a526f9e637e1b5dd5b9127519ff8b3fa9fa1534e8fb4a8853425e47ba0e4762e"
+    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.3/axiom-syslog-proxy_0.1.3_darwin_arm64.tar.gz"
+    sha256 "b8cafca483324dc7a98479e4d8bb73255bd0f5f10e186a9c0444b51c4ffee218"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.1/axiom-syslog-proxy_0.1.1_linux_amd64.tar.gz"
-    sha256 "78f4258fdabca2198f2ab21c1f3871aeb5fa26bfb898315dd180f89fca8e75de"
+    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.3/axiom-syslog-proxy_0.1.3_linux_amd64.tar.gz"
+    sha256 "04882cfd718eadcc78c52659311dfb382e72dd42545122440101b5aabe76fe78"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.1/axiom-syslog-proxy_0.1.1_linux_armv6.tar.gz"
-    sha256 "605c64248f6c4a063caf5af7312a7dd37b54d37028011bd55981695ffca62bfa"
+    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.3/axiom-syslog-proxy_0.1.3_linux_armv6.tar.gz"
+    sha256 "9524c23fc8c238763898df16db16b6ac3d8beb2c31e7830d226093500cceb994"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.1/axiom-syslog-proxy_0.1.1_linux_arm64.tar.gz"
-    sha256 "54117df3ed693ed67ececca593892ef2dfb82b8a77664c5782d7848655b84a06"
+    url "https://github.com/axiomhq/axiom-syslog-proxy/releases/download/v0.1.3/axiom-syslog-proxy_0.1.3_linux_arm64.tar.gz"
+    sha256 "07cc4f6e87513243ab5dc373c44e6e6caeb1e49b5e43c24605d1e71a7fc97f25"
   end
 
   def install
